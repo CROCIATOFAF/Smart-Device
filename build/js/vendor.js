160dc83eb78a1a6872ceefe6ee8a3583214dcd07
CROCIATOFAF <48692205+CROCIATOFAF@users.noreply.github.com>
2020-02-08 21:04:53 +0300

    Добавляет аккордеон и маску для инпутов

--- build/js/vendor.js
+++ build/js/vendor.js
@@ -1,7 +1,12 @@
 'use strict';
 
 var element = document.getElementById('tel');
-var maskOptions = {
-  mask: '+{7}(000)000-00-00'
-};
-var mask = IMask(element, maskOptions);
+var modalPhoneInput = document.getElementById('phone');
+
+if (element && modalPhoneInput) {
+  var maskOptions = {
+    mask: '+{7}(000)000-00-00'
+  };
+  var mask = IMask(element, maskOptions);
+  var maskTwo = IMask(modalPhoneInput, maskOptions);
+}
